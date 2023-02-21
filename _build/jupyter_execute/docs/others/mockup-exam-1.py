432d7ab71825a849940423c3ed9671539d8af469
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2023-02-21 02:41:26 +0100

    Update.

--- _build/jupyter_execute/docs/others/mockup-exam-1.py
+++ _build/jupyter_execute/docs/others/mockup-exam-1.py
@@ -2,32 +2,91 @@
 # coding: utf-8
 
 # # Mockup Exam 1
+# 
+# (Last updated: Feb 21, 2023)
+
+# **Q1:** Which of the following statements about missing data is **FALSE**?
+# 1. Missing Completely At Random (MCAR) means that the missing data is a completely random subset of the entire dataset.
+# 2. Missing data can be treated by replacing them with a constant value.
+# 3. Missing Not At Random (MNAR) cannot be solved simply with imputation.
+# 4. Missing at Random (MAR) means the missing data is related to the variable that has the missing data.
+
+# **Q2:** If data frame A and B both have no missing data, which of the following operations will likely produce missing data?
+# \
+# Operation A: Left join
+# \
+# Operation B: Right join
+# \
+# Operation C: Inner join
+# \
+# Operation D: Outer join
+# 1. Only operation C
+# 2. Only operation D
+# 3. Operation C and D
+# 4. Operation A, B, and D
+
+# **Q3:** Which of the following statements about overfitting is **FALSE**?
+# 1. Overfitting means fitting the test data extremely well.
+# 2. Overfitting usually happens when using a very complex model.
+# 3. We cannot deal with overfitting by simply removing outliers.
+# 4. Errors of the model that we trained can be decomposed into bias, variance, and noise. Overfitting usually happens when we train a model with high variance.
+
+# **Q4:** Which of the following statements about the Decision Tree model is **TRUE**?
+# 1. Decision Tree can be trained well using entropy as the node-splitting strategy.
+# 2. When splitting nodes, Decision Tree cannot use the same feature multiple times.
+# 3. Decision Tree cannot be used for the regression task.
+# 4. Decision Tree can only handle categorical features.
+
+# **Q5:** The research team in a municipality wants to use data science to explain how air pollution affects citizens’ living quality in the city. The municipality requires that the model’s decision-making process is as transparent as possible. This means that a researcher can check the model and explain how the model converts an input to an output intuitively. Which of the following algorithms does **NOT** suit the purpose?
+# 1. Decision Tree
+# 2. Linear Regression
+# 3. Logistic regression
+# 4. Deep Neural Network
+
+# **Q6:** Suppose we flipped a coin 12 times, and we got 10 heads and 2 tails. What is the entropy of the result in this coin-flipping experiment?
+# 1. 0.17
+# 2. 0.20
+# 3. 0.65
+# 4. 0.83
+
+# **Q7:** Suppose we have an array A of data with 3 columns, and we want to put the data in a pandas data frame. We want to give the first column with name "C1", the second column with name "C2", and the third column with name "C3". Which of the following code produces the desired output for array A (considering that we already imported the pandas package)? For example, the output should look like the following data frame:
+# |  | C1 | C2 | C3 |
+# |---|---|---|---|
+# | 0 | 5 | 21.6 | 100 |
+# | 1 | 11 | 48.3 | 213 |
+# | 2 | 1 | 44.2 | 433 |
+# 1. pandas.DataFrame(A, rows=["C1", "C2", "C3"])
+# 2. pandas.DataFrame(A, columns=["C1", "C2", "C3"])
+# 3. pandas.DataFrame(A).set_index(["C1", "C2", "C3"])
+# 4. pandas.DataFrame(A, columns=["C1", "C2", "C3"]).set_index(["C1"])
+
+# **Q8:** Suppose we have a pandas data frame D with 100 rows and one column C1. Column C1 has 25% missing data. We want to sum up all valid items in column C1. Which of the following code produces the desired output? For example, if D looks like the data frame below, the code should output 27, which is a sum of 3, 4, and 20.
+# |  | C1 |
+# |---|---|
+# | 0 | NaN |
+# | 1 | 3 |
+# | 2 | 4 |
+# | 3 | 20 |
+# 1. D.drop("C1", axis=1).sum()
+# 2. D.dropna().sum()["C1"]
+# 3. D.sum("C1")
+# 4. D.groupby("C1").sum()
+
+# # Answers
+
+# The block below contains the answers to the questions.
 
 # In[1]:
 
 
-import pandas as pd
-
-
-# ## Question 1: One-Hot Encoding
-
-# Given a webpage with unknown number of images and a list of images selected by the users, perform one-hot encoding on the selected image IDs.
-
-# In[2]:
-
-
-# Below is an example input.
-data_q1_case1 = [
-[1, ["1","2","10"]],
-[2, ["1"]],
-[3, []],
-[4, ["4","2","11","7","5"]],
-[5, ["4","5","7","1"]],
-[6, ["1","4"]],
-[7, ["6","8"]],
-[8, ["9","4","5"]]]
-data_q1_case1 = pd.DataFrame(data=data_q1_case1, columns=["user_id","image_id_list"]).set_index("user_id")
-data_q1_case1
+# Q1 -> 4
+# Q2 -> 4
+# Q3 -> 1
+# Q4 -> 1
+# Q5 -> 4
+# Q6 -> 3
+# Q7 -> 2
+# Q8 -> 2
 
 
 # In[ ]:
